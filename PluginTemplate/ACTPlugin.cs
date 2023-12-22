3b71f3017cbd0f441caf5c5267707945d7cb0b46
loveloliii <loli@loli.monster>
2023-12-22 16:35:38 +0800

    add IOverlayAddonV2 plugin

--- PluginTemplate/ACTPlugin.cs
+++ PluginTemplate/ACTPlugin.cs
@@ -1,13 +1,16 @@
 ﻿using Advanced_Combat_Tracker;
 using CatPlugin;
 using Lotlab.PluginCommon.FFXIV;
+using Newtonsoft.Json.Linq;
 using PluginTemplate;
+using RainbowMage.OverlayPlugin;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using System.Windows.Forms.Integration;
 
 namespace CatTemplate
 {
-    public partial class ACTPlugin : IActPluginV1
+    public partial class PluginCatTemplate : IActPluginV1, IOverlayAddonV2
     {
         /// <summary>
         /// FFXIV 插件引用
@@ -39,6 +42,7 @@ namespace CatTemplate
                 if (ACTPluginProxy.IsFFXIVPlugin(item.pluginObj))
                 {
                     ffxiv = new ACTPluginProxy(item.pluginObj);
+
                     break;
                 }
             }
@@ -51,7 +55,10 @@ namespace CatTemplate
             }
 
             // 注册网络事件
+            // Register events
+            ffxiv.DataSubscription.NetworkSent += onNetworkSend;
             ffxiv.DataSubscription.NetworkReceived += onNetworkReceived;
+            ffxiv.DataSubscription.LogLine += onLogLine;
 
             // 初始化UI
             var vm = new PluginControlViewModel();
@@ -99,6 +106,118 @@ namespace CatTemplate
         void onNetworkReceived(string connection, long epoch, byte[] message)
         {
             // todo
+        }
+
+        private void onLogLine(uint EventType, uint Seconds, string logline)
+        {
+            if (EventType == 0x00)
+            {
+
+            }
+        }
+        private void onNetworkSend(string connection, long epoch, byte[] message)
+        {
+
+        }
+
+        public void Init()
+        {
+
+            var container = Registry.GetContainer();
+            var registry = container.Resolve<Registry>();
+
+            // 注册事件源
+            var eventSource = new TemplateEventSource(container);
+            //registry.StartEventSource(eventSource);
+            var method = registry.GetType().GetMethod(nameof(registry.StartEventSource));
+            if (method.IsGenericMethod)
+                method = method.MakeGenericMethod(typeof(TemplateEventSource));
+
+            method.Invoke(registry, new object[] { eventSource });
+            // 注册悬浮窗预设
+            /*registry.RegisterOverlayPreset2(new OverlayPreset
+            {
+                Name = "喵",
+                Url = "http://localhost:5173/",
+                Size = new int[] { 300, 500 },
+                Locked = false,
+            });*/
+            registry.RegisterOverlayPreset2(new OverlayPreset
+            {
+                Name = "喵",
+                Url = "http://localhost:5173/",
+                Size = new int[] { 300, 500 },
+                Locked = false,
+            });
+
+
+        }
+
+        /// <summary>
+        /// 悬浮窗事件源
+        /// </summary>
+        public class TemplateEventSource : Lotlab.PluginCommon.Overlay.EventSourceBase
+        {
+            const string SAMPLE_EVENT = "templateSampleEvent";
+
+            public TemplateEventSource(TinyIoCContainer c) : base(c)
+            {
+                // 设置事件源名称，必须是唯一的
+                Name = "喵";
+
+                // 注册数据源名称。
+                // 此数据源提供给悬浮窗监听，悬浮窗使用addOverlayListener注册对应事件的回调。
+                RegisterEventTypes(new List<string>()
+            {
+                SAMPLE_EVENT
+            });
+
+                // 注册事件接收器。
+                // 注册完毕后，悬浮窗可以使用callOverlayHandler调用已经注册的方法
+                RegisterEventHandler("templateSampleFunction", (obj) =>
+                {
+                    return JObject.FromObject(new
+                    {
+                        message = "Hello, world."
+                    });
+                });
+            }
+            public override Control CreateConfigControl()
+            {
+                return null;
+            }
+
+            public override void LoadConfig(IPluginConfig config)
+            {
+            }
+
+            public override void SaveConfig(IPluginConfig config)
+            {
+            }
+
+            public void InvokeSampleEvent(string data)
+            {
+                // 将数据发送给悬浮窗
+                DispatchEvent(JObject.FromObject(new
+                {
+                    type = SAMPLE_EVENT,
+                    data = data
+                }));
+            }
+        }
+
+        /// <summary>
+        /// 悬浮窗预设
+        /// </summary>
+        struct OverlayPreset : IOverlayPreset
+        {
+            public string Name { get; set; }
+            public string Type { get { return "MiniParse"; } }
+            public string Url { get; set; }
+            public int[] Size { get; set; }
+            public bool Locked { get; set; }
+            public List<string> Supports { get { return new List<string> { "modern" }; } }
+
         }
     }
 }
--- PluginTemplate/OverlayPlugin.cs
+++ PluginTemplate/OverlayPlugin.cs
@@ -1,8 +1,11 @@
-﻿using Lotlab.PluginCommon.Overlay;
+﻿/*using Lotlab.PluginCommon.Overlay;
 using Newtonsoft.Json.Linq;
 using RainbowMage.OverlayPlugin;
+using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Windows.Media.Media3D;
+using System.Xml.Linq;
 
 namespace CatPlugin
 {
@@ -12,7 +15,7 @@ namespace CatPlugin
     /// <remarks>
     /// 若不需要悬浮窗，则可直接删除此文件和对应的依赖项目
     /// </remarks>
-    public partial class ACTPlugin : IOverlayAddonV2
+    public partial class PluginCatTemplate : IOverlayAddonV2
     {
         void IOverlayAddonV2.Init()
         {
@@ -21,24 +24,44 @@ namespace CatPlugin
 
             // 注册事件源
             var eventSource = new TemplateEventSource(container);
-            registry.StartEventSource(eventSource);
+            //registry.StartEventSource(eventSource);
+            var method = registry.GetType().GetMethod(nameof(registry.StartEventSource));
+            if (method.IsGenericMethod)
+                method = method.MakeGenericMethod(typeof(TemplateEventSource));
 
+            method.Invoke(registry, new object[] { eventSource });
             // 注册悬浮窗预设
-            registry.RegisterOverlayPreset2(new TemplateOverlayPresent());
+            registry.RegisterOverlayPreset2(new OverlayPreset
+            {
+                Name = "喵",
+                Url = "http://localhost:5173/",
+                Size = new int[] { 300, 500 },
+                Locked = false,
+            });
+            registry.RegisterOverlayPreset2(new OverlayPreset
+            {
+                Name = "喵",
+                Url = "http://localhost:5173/",
+                Size = new int[] { 300, 500 },
+                Locked = false,
+            });
+
+
         }
+
     }
 
     /// <summary>
     /// 悬浮窗事件源
     /// </summary>
-    public class TemplateEventSource : EventSourceBase
+    public class TemplateEventSource : Lotlab.PluginCommon.Overlay.EventSourceBase
     {
         const string SAMPLE_EVENT = "templateSampleEvent";
 
         public TemplateEventSource(TinyIoCContainer c) : base(c)
         {
             // 设置事件源名称，必须是唯一的
-            Name = "TemplatePluginES";
+            Name = "喵";
 
             // 注册数据源名称。
             // 此数据源提供给悬浮窗监听，悬浮窗使用addOverlayListener注册对应事件的回调。
@@ -84,23 +107,15 @@ namespace CatPlugin
     /// <summary>
     /// 悬浮窗预设
     /// </summary>
-    public class TemplateOverlayPresent : IOverlayPreset
+    struct OverlayPreset : IOverlayPreset
     {
-        string IOverlayPreset.Name => "悬浮窗模板";
+        public string Name { get; set; }
+        public string Type { get { return "MiniParse"; } }
+        public string Url { get; set; }
+        public int[] Size { get; set; }
+        public bool Locked { get; set; }
+        public List<string> Supports { get { return new List<string> { "modern" }; } }
 
-        string IOverlayPreset.Type => "MiniParse";
-
-        string IOverlayPreset.Url => "https://example.org";
-
-        int[] IOverlayPreset.Size => new int[2] { 200, 200 };
-
-        bool IOverlayPreset.Locked => false;
-
-        List<string> IOverlayPreset.Supports => new List<string> { "modern" };
-
-        public override string ToString()
-        {
-            return ((IOverlayPreset)this).Name;
-        }
     }
 }
+*/
